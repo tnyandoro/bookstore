ba1f788bfabb86bd82609e007d049cd1723b9699
Tendai <tendai1@hotmail.com>
2021-08-30 20:50:11 +0200

    Redux folder

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function books() {
   return (
-    <div>
-      
-    </div>
-  )
+    <div />
+  );
 }
 
-export default books
+export default books;

2bdfa11f40b29ba55ea57208327d708993a8939e
Tendai <tendai1@hotmail.com>
2021-09-03 13:07:33 +0200

    main style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import Nav from './components/Nav';
 import store from './redux/configureStore';
-import './App.css';
 
 function App() {
   return (
